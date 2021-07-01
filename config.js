9a0b0ab74c4e97fab9e270f443d5e9390eb19f09
ddoongne03 <86699608+ddoongne03@users.noreply.github.com>
2021-07-01 13:24:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,14 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hi cacban :)))',
+    btnIntro: 'ấn đây nè',
+    title: 'hết ngày 8/7 là thi xong rồi đó',
+    desc: 'sắp chia tay mọi người muốn mời tổ trưởng đi ăn đúng không ',
+    btnYes: 'muốn lâu rồi :))',
+    btnNo: 'tổ trưởng mời đeeee',
+    question:'tại sao cacban cứ nhất quyết mời mình vậy ',
+    btnReply: 'mời tổ trưởng',
+    reply: 'tổ trưởng tốt bụng',
+    mess: 'cacban có lòng thì tớ cũng có dạ.đành phải đi vậy',
+    btnAccept: 'Oke lun ',
+    messLink: 'https://www.facebook.com/profile.php?id=100025350827065
 }
